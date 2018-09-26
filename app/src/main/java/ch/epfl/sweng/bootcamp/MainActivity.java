19caa7ac060b681405fa3a9c82e4c728f7d682c9
Unknown <julie.giunta@epfl.ch>
2018-09-26 16:15:27 +0200

    End bootcamp

--- app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -1,13 +1,26 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "ch.epfl.sweng.bootcamp.greetingMessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**Called when the user taps the Enter button */
+    public void showGreetings(View view) {
+        Intent intent = new Intent(this, GreetingActivity.class);
+        EditText editText = findViewById(R.id.mainName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
